5108cfe215a7931d2e26cd3d1e12d4ed7da509e0
ryanwade0914 <ryan.wade04@gmail.com>
2021-05-19 15:56:18 -0500

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM python:3.7.5-slim
+RUN python -m pip install DateTime
+COPY ./test/test.py /home
---
+++ test/test.py
@@ -0,0 +1,5 @@
+print("Hello world, from Python")
+
+print("First change.")
+
+print("Second change.")
